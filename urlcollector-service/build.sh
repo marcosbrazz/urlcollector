24a8e02bbe3a363d80e9ce25714edb6d707a0f2f
marcosbrazz <marcos.brazz@gmail.com>
2018-03-14 00:47:18 -0300

    Creating README and build.sh

--- urlcollector-service/build.sh
+++ urlcollector-service/build.sh
@@ -1,2 +1,27 @@
-echo "START BUILD.SH"
-echo "END BUILD.SH"
+#!/bin/bash
+
+if [[ ! -r $2  || -z $1 ]]
+		then
+			echo "Incorrect usage, See README"
+			exit 1
+fi
+
+NAMESPACE=$1
+API_KEY_FILE=$2
+
+bx login --apikey @$API_KEY_FILE
+
+echo "Build docker image"
+docker build -t registry.ng.bluemix.net/$NAMESPACE/compute-interest-api .
+
+echo "Push docker image"
+docker push registry.ng.bluemix.net/$NAMESPACE/compute-interest-api
+
+echo "Create resource on Kubernetes Cluster"
+kubectl create -f urlcollector-service.yaml
+
+# echo $?  last command exit code
+
+echo "DONE"
+
+exit 0
