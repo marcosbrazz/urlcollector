a931b99658ac7480c4268efabc84a305fc667cba
marcosbrazz <marcos.brazz@gmail.com>
2018-03-15 00:13:36 -0300

    Finalizado readme; finalizado build script

--- urlcollector-service/build.sh
+++ urlcollector-service/build.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-if [[ ! -r $2  || -z $1 ]]
+while getopts n:k:r:c: option
+do
+	case "${option}"
+		in
+		n) NAMESPACE=${OPTARG};;
+		k) API_KEY_FILE=${OPTARG};;
+		r) REGION=${OPTARG};;
+		c) CLUSTER_NAME=${OPTARG};;
+		
+	esac
+done
+
+if [[ ! -r $API_KEY_FILE  || -z $NAMESPACE ||  -z $REGION || -z $CLUSTER_NAME ]]
 		then
 			echo "Incorrect usage, See README"
 			exit 1
 fi
 
-NAMESPACE=$1
-API_KEY_FILE=$2
 
-bx login --apikey @$API_KEY_FILE
+bx plugin install container-service -r Bluemix -f &&
+bx login --apikey @$API_KEY_FILE &&
+bx cs region-set $REGION &&
+$(bx cs cluster-config --export $CLUSTER_NAME) &&
 
 echo "Build docker image"
-docker build -t registry.ng.bluemix.net/$NAMESPACE/compute-interest-api .
+docker build -t registry.ng.bluemix.net/$NAMESPACE/url-collector-service . &&
 
 echo "Push docker image"
-docker push registry.ng.bluemix.net/$NAMESPACE/compute-interest-api
+docker push registry.ng.bluemix.net/$NAMESPACE/url-collector-service &&
 
 echo "Create resource on Kubernetes Cluster"
 kubectl create -f urlcollector-service.yaml
 
-# echo $?  last command exit code
-
-echo "DONE"
-
-exit 0
+exit $?
